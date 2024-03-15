9148dab8e532ea87669190256386d54684340c39
not-a-no <19555822+not-a-no@users.noreply.github.com>
2024-03-15 16:03:16 -0400

    edit lists_in_python.py

---
+++ lists_in_python.py
@@ -0,0 +1,13 @@
+#[]indicate a list
+bicycles = ['trek','cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#index
+print(bicycles[0])
+print(bicycles[0].title())
+
+#special syntax for accessing the last elements in a list, index -1
+print(bicycles[-1])
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
