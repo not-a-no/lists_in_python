ce70d0e9d1e1c21d20c9ce1a5b66d2c6cffa2092
not-a-no <19555822+not-a-no@users.noreply.github.com>
2024-03-19 22:30:55 -0400

    Edit Lists_in_python.py

--- lists_in_python.py
+++ lists_in_python.py
@@ -40,11 +40,31 @@ print(motorcycles)
 motorcycles = ['honda', 'yamaha', 'suzuki']
 del motorcycles[0]
 print(motorcycles)
+
 #removing an item with the pop() method
 #the pop() method removes the last item in a list
 #but it lets you work with that item after removing
-
 motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
 popped_motorcycle = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycle)
+print(f"The last motorcycle I owned was a {popped_motorcycle.title()}.")
+
+#Popping items from any position in a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+#Removing an item by value
+#can also work with a value that's being removed
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
